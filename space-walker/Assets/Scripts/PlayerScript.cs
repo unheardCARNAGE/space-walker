1101922ac4788458509ec9926fabdb4516b02b15
Jon Apostol <jrcapostol@gmail.com>
2017-01-20 23:59:06 -0800

    Added Ray Casting to PlayerScript.cs
    Still currently working with ray casting.
    The Debug.DrawLine now works

--- space-walker/Assets/Scripts/PlayerScript.cs
+++ space-walker/Assets/Scripts/PlayerScript.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 
+    public int health = 100;
     public float movementSpeed = 0.05f;
+    public float maxRayDistance = 200.0f;
 
-	// Use this for initialization
-	void Start () {
+    public Transform light;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    void FixedUpdate()
+    {
+        //Ray casting
+
+        Ray ray = new Ray(transform.position, -light.transform.forward);
+
+        RaycastHit hit;
+
+        //Debug.DrawLine()
+        Debug.DrawLine(transform.position, transform.position + Vector3.forward * maxRayDistance + Vector3.up * maxRayDistance, Color.red, 100f, false);
+
+        if (Physics.Raycast(ray, out hit, maxRayDistance))
+        {
+            Debug.Log("Player dies");
+            Debug.DrawLine(transform.position, transform.position + Vector3.forward * maxRayDistance, Color.blue, 100f, false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
 
         //Movement
         Vector3 newPos = transform.position;
