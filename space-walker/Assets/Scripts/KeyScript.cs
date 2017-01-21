3771d4595e5d28999f2fbd33834f792864a82e12
Jon Apostol <jrcapostol@gmail.com>
2017-01-21 15:57:50 -0800

    Adding KeyScript
    Created a new class: KeyScript.cs. This script determines the key's
    behavior when the player grabs the key and uses it for a specific
    button.

---
+++ space-walker/Assets/Scripts/KeyScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.name == "Player")
+        {
+            Debug.Log("player has the key");
+            Destroy(gameObject, 0.5f);
+        }
+    }
+}
