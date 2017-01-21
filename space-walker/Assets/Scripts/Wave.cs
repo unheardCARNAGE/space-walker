989a36a1304bc0d83672eda5d8bb0cc075b6bdfd
Rikerd <cursedmarkedsasuke@gmail.com>
2017-01-20 22:16:25 -0800

    Prototype Wave Script
    Wave Script with timer working

--- space-walker/Assets/Scripts/Wave.cs
+++ space-walker/Assets/Scripts/Wave.cs
@@ -3,14 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Wave : MonoBehaviour {
+    public float timer = 30f;
+    public float resetTimer = 30f;     
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        Invoke("Warning", timer - 5);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0f)
+        {
+            //turn player raycast on
+            //start wave animation
+            //time to keep raycast and wave anim up
+            timer = resetTimer;
+            Invoke("Warning", timer - 5);
+            //turn player raycast off
+            //end wave animation
+        }
+    }
+
+    void Warning()
+    {
+        Debug.Log("Warning");
+    }
 }
