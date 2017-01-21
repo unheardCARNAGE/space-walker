6f4c009b22e04ac2155b47f1a34f9a829c0f2f0c
unheardCARNAGE <timcharveyjr@hotmail.com>
2017-01-20 22:25:42 -0800

    Added button script
    The button script works in the sense it moves the object in any given
    direction, but have not implemented a time limit yet.

---
+++ space-walker/Assets/Scripts/ButtonTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTrigger : MonoBehaviour {
+
+    public float movementSpeed;
+    public float xdir;
+    public float ydir;
+    public float zdir;
+    //public float deltaTime;
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    transform.Translate(new Vector3 (xdir, ydir, zdir) * movementSpeed * Time.deltaTime);
+		
+	}
+}
