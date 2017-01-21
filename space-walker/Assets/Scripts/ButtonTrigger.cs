b2bde29f494617aaa81f477c5d5cc85ecab12e0c
unheardCARNAGE <timcharveyjr@hotmail.com>
2017-01-20 22:35:32 -0800

    Modified button script
    GOing to use lerp, but currently does not work.

--- space-walker/Assets/Scripts/ButtonTrigger.cs
+++ space-walker/Assets/Scripts/ButtonTrigger.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ButtonTrigger : MonoBehaviour {
 
+    public GameObject;
     public float movementSpeed;
     public float xdir;
     public float ydir;
     public float zdir;
+    Transform startMarker = GameObject.GetComponent("Transform");
     //public float deltaTime;
     // Use this for initialization
     void Start () {
