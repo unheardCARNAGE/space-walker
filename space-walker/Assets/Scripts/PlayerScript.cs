c6d042de77fc7e9532cdcbc5df06ec909bf4b953
Jon Apostol <jrcapostol@gmail.com>
2017-01-20 21:27:17 -0800

    Updated and Uploaded PlayerScript
    PlayerScript can now move player. The PlayerScript is located in the
    Script folder.

---
+++ space-walker/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public float movementSpeed = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Movement
+        Vector3 newPos = transform.position;
+
+        //Up
+            if (Input.GetKey(KeyCode.W))
+            {
+                newPos += new Vector3(
+                    0,
+                    0,
+                    movementSpeed
+                    );
+            }
+        //Left
+            if (Input.GetKey(KeyCode.A))
+            {
+                newPos += new Vector3(
+                    -movementSpeed,
+                    0,
+                    0);
+            }
+        //Down
+            if (Input.GetKey(KeyCode.S))
+            {
+                newPos += new Vector3(
+                    0,
+                    0,
+                    -movementSpeed);
+            }
+        //Up
+            if (Input.GetKey(KeyCode.D))
+            {
+                newPos += new Vector3(
+                    movementSpeed,
+                    0,
+                    0);
+            }
+
+        transform.position = newPos;
+	}
+}
