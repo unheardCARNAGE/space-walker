64252ba7375ca61b55794ec7b0d3ade045207bca
Rikerd <cursedmarkedsasuke@gmail.com>
2017-01-21 12:38:03 -0800

    Rotation Button + Bridge
    Rotation button and bridge added

---
+++ space-walker/Assets/Scripts/ObstacleScripts/RotationButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationButton : MonoBehaviour {
+    public GameObject[] obstacleAry;
+    public bool clickable;
+
+    void OnMouseUp()
+    {
+        if (!clickable)
+        {
+            return;
+        }
+
+        foreach (GameObject obstacle in obstacleAry)
+        {
+            if (!obstacle.GetComponent<RotationScript>().pressed)
+            {
+                obstacle.GetComponent<RotationScript>().pressed = true;
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.name == "Player")
+        {
+            clickable = true;
+        }
+    }
+
+    void OnTriggerExit(Collider coll)
+    {
+        if (coll.gameObject.name == "Player")
+        {
+            clickable = false;
+        }
+    }
+}
---
+++ space-walker/Assets/Scripts/ObstacleScripts/RotationScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour {
+    public float yRot = 90f;
+    public float smooth = 3f;
+    public bool pressed = false;
+    public float timePassed = 0f;
+
+    public Quaternion originalRot;
+
+    void Start()
+    {
+        originalRot = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pressed)
+        {
+            timePassed += Time.deltaTime;
+            transform.rotation = Quaternion.Slerp(originalRot, Quaternion.Euler(0, originalRot.eulerAngles.y + yRot, 0), timePassed);
+
+            Debug.Log(transform.rotation.eulerAngles.y);
+
+            if (timePassed >= 1f)
+            {
+                originalRot = transform.rotation;
+                timePassed = 0f;
+                pressed = false;
+            }
+        }
+    }
+}
