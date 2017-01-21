35554e83e0d144f57ac507f895d1b899ae1892a2
Rikerd <cursedmarkedsasuke@gmail.com>
2017-01-21 12:53:45 -0800

    Updated Wave
    Updated wave script

--- space-walker/Assets/Scripts/ShockwaveScript.cs
+++ space-walker/Assets/Scripts/ShockwaveScript.cs
@@ -1,28 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Light))]
 
 public class ShockwaveScript : MonoBehaviour {
     [SerializeField]
-    float cycle = 8f;
+    float totalCycleTime = 8f;
     [SerializeField]
     float warmupTime = 5f;
     [SerializeField]
     float flashDuration = 0.5f;
     [SerializeField]
     float cooldown = 0.5f;
+
+    [SerializeField]
+    Color normalColor = Color.white;
+    [SerializeField]
+    float normalBrightness = 1f;
+    [SerializeField]
+    Color warmedUpColor = Color.red;
     [SerializeField]
     float warmedUpBrightness = 2f;
     [SerializeField]
+    Color flashColor = Color.red;
+    [SerializeField]
     float flashBrightness = 4f;
+
+    [SerializeField]
+    UnityEvent onFlashEvent;
 
     Light light;
     float currentTime = 0f;
-
-    public float timer = 30f;
-    public float resetTimer = 30f;
+    bool flashpointDoneThisCycle = false;
 
     void Start()
     {
@@ -32,39 +43,72 @@ public class ShockwaveScript : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        timer -= Time.deltaTime;
-
-        if (timer <= cycle)
-        {
-            currentTime += Time.deltaTime;
-            ChangeBrightness();
-            if (timer <= 0)
-            {
-                timer = resetTimer;
-            }
-        }
+        currentTime += Time.deltaTime;
+        ChangeBrightness();
     }
 
     void ChangeBrightness()
     {
-        float brightness = 0f;
-        if (currentTime >= cycle - (warmupTime + flashDuration) && currentTime < cycle - flashDuration)
+        if (currentTime < warmupTime)
         {
-            brightness = warmedUpBrightness * (currentTime - (cycle - (warmupTime + flashDuration))) / warmupTime;
+            //warmup mode
+            WarmupBrightness();
         }
-        else if (currentTime >= cycle - flashDuration && currentTime < cycle)
+        else if (currentTime < warmupTime + flashDuration)
         {
-            brightness = FlashBrightness();
+            //flash mode
+            FlashBrightness();
         }
-        else if (currentTime >= cycle)
+        else if (currentTime < warmupTime + flashDuration + cooldown)
         {
+            //cooldown mode
+            CooldownBrightness();
+            //this sends an event as soon as the flash ends; IE, when it reaches full brightness.
+            if (!flashpointDoneThisCycle)
+            {
+                FlashResponse();
+                flashpointDoneThisCycle = true;
+            }
+        }
+        else if (currentTime < totalCycleTime)
+        {
+            //waiting mode
+        }
+        else
+        {
+            //restart cycle
             currentTime = 0f;
+            flashpointDoneThisCycle = false;
         }
-        light.intensity = brightness;
     }
 
-    float FlashBrightness()
+    void WarmupBrightness()
     {
-        return warmedUpBrightness + ((flashBrightness - warmedUpBrightness) * (currentTime - (cycle - flashDuration)) / flashDuration);
+        float percentTime = currentTime / warmupTime;
+        LerpColorAndBrightness(normalColor, warmedUpColor, normalBrightness, warmedUpBrightness, percentTime);
+    }
+
+    void FlashBrightness()
+    {
+        float percentTime = (currentTime - warmupTime) / flashDuration;
+        LerpColorAndBrightness(warmedUpColor, flashColor, warmedUpBrightness, flashBrightness, percentTime);
+    }
+
+    void CooldownBrightness()
+    {
+        float percentTime = (currentTime - (warmupTime + flashDuration)) / cooldown;
+        LerpColorAndBrightness(flashColor, normalColor, flashBrightness, normalBrightness, percentTime);
+    }
+
+    void LerpColorAndBrightness(Color startC, Color endC, float startB, float endB, float percentTime)
+    {
+        light.color = Color.Lerp(startC, endC, percentTime);
+        light.intensity = Mathf.Lerp(startB, endB, percentTime);
+    }
+
+    void FlashResponse()
+    {
+        if (onFlashEvent != null)
+            onFlashEvent.Invoke();
     }
 }
