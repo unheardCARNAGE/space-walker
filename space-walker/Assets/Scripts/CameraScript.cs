0c25f9144ed0009d294ee9ce14b106b2ed46fdb4
Rikerd <cursedmarkedsasuke@gmail.com>
2017-01-20 23:59:47 -0800

    Shockwave + Camera
    Shockwave Timer and Camera movement

---
+++ space-walker/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+    public Transform player;
+    public float offset = 25f;
+    public float smoothTime = 0.3f;
+
+    private Vector3 velocity = Vector3.zero;
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 goalPos = player.position;
+        goalPos.z = player.position.z - offset;
+        goalPos.y = transform.position.y;
+
+        transform.position = Vector3.SmoothDamp(transform.position, goalPos, ref velocity, smoothTime);
+	}
+}
---
+++ space-walker/Assets/Scripts/ShockwaveScript.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+
+public class ShockwaveScript : MonoBehaviour {
+    [SerializeField]
+    float cycle = 8f;
+    [SerializeField]
+    float warmupTime = 5f;
+    [SerializeField]
+    float flashDuration = 0.5f;
+    [SerializeField]
+    float cooldown = 0.5f;
+    [SerializeField]
+    float warmedUpBrightness = 2f;
+    [SerializeField]
+    float flashBrightness = 4f;
+
+    Light light;
+    float currentTime = 0f;
+
+    public float timer = 30f;
+    public float resetTimer = 30f;
+
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= cycle)
+        {
+            currentTime += Time.deltaTime;
+            ChangeBrightness();
+            if (timer <= 0)
+            {
+                timer = resetTimer;
+            }
+        }
+    }
+
+    void ChangeBrightness()
+    {
+        float brightness = 0f;
+        if (currentTime >= cycle - (warmupTime + flashDuration) && currentTime < cycle - flashDuration)
+        {
+            brightness = warmedUpBrightness * (currentTime - (cycle - (warmupTime + flashDuration))) / warmupTime;
+        }
+        else if (currentTime >= cycle - flashDuration && currentTime < cycle)
+        {
+            brightness = FlashBrightness();
+        }
+        else if (currentTime >= cycle)
+        {
+            currentTime = 0f;
+        }
+        light.intensity = brightness;
+    }
+
+    float FlashBrightness()
+    {
+        return warmedUpBrightness + ((flashBrightness - warmedUpBrightness) * (currentTime - (cycle - flashDuration)) / flashDuration);
+    }
+}
