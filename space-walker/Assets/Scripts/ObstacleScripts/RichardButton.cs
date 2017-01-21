9fa018cf333e723cc8e0eaa662d56f32c8bdd736
Rikerd <cursedmarkedsasuke@gmail.com>
2017-01-21 08:16:38 -0800

    Button Range
    Player must be in range in order to use button

--- space-walker/Assets/Scripts/ObstacleScripts/RichardButton.cs
+++ space-walker/Assets/Scripts/ObstacleScripts/RichardButton.cs
@@ -4,14 +4,36 @@ using UnityEngine;
 
 public class RichardButton : MonoBehaviour {
     public GameObject[] obstacleAry;
+    public bool clickable;
     
     void OnMouseDown()
     {
+        if (!clickable)
+        {
+            return;
+        }
+
         foreach (GameObject obstacle in obstacleAry) {
             if (!obstacle.GetComponent<ObstacleActivated>().active)
             {
                 obstacle.GetComponent<ObstacleActivated>().active = true;
             }
+        }
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.name == "Player")
+        {
+            clickable = true;
+        }
+    }
+
+    void OnTriggerExit(Collider coll)
+    {
+        if (coll.gameObject.name == "Player")
+        {
+            clickable = false;
         }
     }
 }
