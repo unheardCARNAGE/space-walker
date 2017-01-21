09b710bacd81320ce8ada4f0bfaad8e3a504b17c
Rikerd <cursedmarkedsasuke@gmail.com>
2017-01-21 08:59:00 -0800

    Obstacle Script
    Combined all the obstacle scripts into one

--- space-walker/Assets/Scripts/ObstacleScripts/ObstacleScript.cs
+++ space-walker/Assets/Scripts/ObstacleScripts/ObstacleScript.cs
@@ -2,24 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorScript : MonoBehaviour {
+public class ObstacleScript : MonoBehaviour {
     public float xDir;
+    public float yDir;
     public float zDir;
-    public float movementSpeed;
+    public float movementSpeed = 3f;
 
     private float timePassed;
     private Vector3 originalPos;
 
-    public bool open;
+    public bool active;
 
     // Use this for initialization
     void Start()
     {
         originalPos = transform.position;
 
-        if (open)
+        if (active)
         {
-            originalPos -= new Vector3(xDir, 0, zDir);
+            originalPos -= new Vector3(xDir, yDir, zDir);
         }
     }
 
@@ -28,21 +29,21 @@ public class DoorScript : MonoBehaviour {
     {
         if (GetComponent<ObstacleActivated>().active)
         {
-            if (open)
+            if (active)
             {
-                transform.position = new Vector3(Mathf.Lerp(originalPos.x + xDir, originalPos.x, timePassed), originalPos.y, Mathf.Lerp(originalPos.z + zDir, originalPos.z, timePassed));
+                transform.position = new Vector3(Mathf.Lerp(originalPos.x + xDir, originalPos.x, timePassed), Mathf.Lerp(originalPos.y + yDir, originalPos.y, timePassed), Mathf.Lerp(originalPos.z + zDir, originalPos.z, timePassed));
                 timePassed += movementSpeed * Time.deltaTime;
             }
             else
             {
-                transform.position = new Vector3(Mathf.Lerp(originalPos.x, originalPos.x + xDir, timePassed), originalPos.y, Mathf.Lerp(originalPos.z, originalPos.z + zDir, timePassed));
+                transform.position = new Vector3(Mathf.Lerp(originalPos.x, originalPos.x + xDir, timePassed), Mathf.Lerp(originalPos.y, originalPos.y + yDir, timePassed), Mathf.Lerp(originalPos.z, originalPos.z + zDir, timePassed));
                 timePassed += movementSpeed * Time.deltaTime;
             }
 
             if (timePassed > 1.0f)
             {
                 GetComponent<ObstacleActivated>().active = false;
-                open = !open;
+                active = !active;
                 timePassed = 0.0f;
             }
         }
