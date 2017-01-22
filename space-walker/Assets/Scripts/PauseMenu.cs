ddd415d6f009e457aab9e69a85e9e9664e495694
unheardCARNAGE <timcharveyjr@hotmail.com>
2017-01-21 21:16:08 -0800

    Addded pause menuscript

---
+++ space-walker/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject menu; // Assign in inspector
+    private bool isShowing;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            isShowing = !isShowing;
+            menu.SetActive(isShowing);
+        }
+    }
+}
