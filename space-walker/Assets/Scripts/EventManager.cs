190d63d9064f859e3f822ba38d9efc9eca7885f9
unheardCARNAGE <timcharveyjr@hotmail.com>
2017-01-21 16:02:31 -0800

    Prototype main menu
    Created a main menu that can load up any scene we tell it too, really
    basic.

---
+++ space-walker/Assets/Scripts/EventManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class EventManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+  public void loadNext() {
+        SceneManager.LoadScene("TimothyHarvey");
+  }
+
+
+  public void loadMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+  public void quitGame() {
+    Application.Quit();
+  }
+}
